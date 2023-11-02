a1bc5760777bb819fffd731cc8cc2512b57421e1
Manish Gupta <117648576+ManishGupta03@users.noreply.github.com>
2023-11-02 20:37:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 //complete this code
 class Rectangle {
 	constructor(width,height){
-		this.width = width;
-		this.height = height;
+		this._width = width;
+		this._height = height;
 	}
 	set width(width){
-		this.width = width;
+		this._width = width;
 	}
 	get width(){
-		return this.width;
+		return this._width;
 	}
 		get height(){
-		return this.height;
+		return this._height;
 	}
 	getArea() {
-    return this.width * this.height;
+    return this._width * this._height;
   }
 }
 
